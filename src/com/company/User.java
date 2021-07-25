feb624acea6b681567865c0285ba0d5c8ed15d2f
PavelRovba <paul.the.hater@gmail.com>
2021-07-25 22:45:38 +0300

    Serialisation deleted.

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -12,7 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
-public class User implements Serializable {
+public class User{
     private String nickname;
     private int rating;
     private List<String> answers;
