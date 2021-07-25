1a1199a4ff23225355dbf5ba1e83b252d5f5ada8
PavelRovba <paul.the.hater@gmail.com>
2021-07-25 23:29:51 +0300

    Method initQuiz changed.

--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,6 +19,7 @@ public class Quiz {
         this.user = user;
     }
 
+
     private void loadQuestions() throws IOException {
         byte[] file = Files.readAllBytes(Paths.get("question.txt"));
         ObjectMapper objectMapper = new ObjectMapper();
@@ -27,17 +29,19 @@ public class Quiz {
     private void initQuiz(){
         int counter = 0;
         String userAnswer;
+        List <String> userAnswers = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < questions.size(); i++) {
             System.out.println(i + " question:\n " + questions.get(i).getFormulation);
             userAnswer = scanner.nextLine();
-
+            userAnswers.add(i,userAnswer);
             if (checkAnswer(userAnswer,i))
                 counter++;
         }
+        user.setAnswers(userAnswers);
         user.setRating(counter*10/questions.size());
         scanner.close();
-
+        user.saveToFile(user);
     }
     private boolean checkAnswer(String userAnswer, int i){
         if (userAnswer.equals(questions.get(i).getCorrectAnswer))
