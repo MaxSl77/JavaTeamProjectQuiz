abd37c4b96a27f63df696c23a9f403b95444f065
Demyanchik Andrey <Andrdemyan4ik@yandex.ru>
2021-07-28 17:57:02 +0300

    Minor fix and code refactor.

--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -3,6 +3,7 @@ package com.company;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,15 +14,15 @@ import java.util.Scanner;
 
 public class Quiz {
     private List<Question> questions;
-    private User user;
+    private User player;
 
     public Quiz(User user) {
-        this.user = user;
+        this.player = user;
     }
 
 
     private void loadQuestions() throws IOException {
-        byte[] file = Files.readAllBytes(Paths.get("question.txt"));
+        File file = new File("questions.json");
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         questions = Arrays.asList(objectMapper.readValue(file, Question[].class));
@@ -32,21 +33,19 @@ public class Quiz {
         List <String> userAnswers = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < questions.size(); i++) {
-            System.out.println(i + " question:\n " + questions.get(i).getFormulation);
+            System.out.println(i + " question:\n " + questions.get(i).getFormulation());
             userAnswer = scanner.nextLine();
             userAnswers.add(i,userAnswer);
             if (checkAnswer(userAnswer,i))
                 counter++;
         }
-        user.setAnswers(userAnswers);
-        user.setRating(counter*10/questions.size());
+        player.setAnswers(userAnswers);
+        player.setRating(counter*10/questions.size());
         scanner.close();
-        user.saveToFile(user);
+        player.saveToFile("users.json");
     }
     private boolean checkAnswer(String userAnswer, int i){
-        if (userAnswer.equals(questions.get(i).getCorrectAnswer))
-            return true;
-        else return false;
+        return userAnswer.equals(questions.get(i).getCorrectAnswer());
     }
 
 }
